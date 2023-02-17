6516b1d82ff88d6dc833ee37a3565d61cfc8fa02
Nullzero <p4rlx-news@pm.me>
2023-02-17 13:30:20 +1030

    stopping for now, too advanced

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #Image captioning using a Encoder-Decoder with Keras
+#https://iq.opengenus.org/image-captioning-using-keras/
 
 #Load captions
 def load_doc(filename):
@@ -8,13 +9,43 @@ def load_doc(filename):
 
 filename = "Flickr8k_text/Flickr8k.token.txt" ### UPDATE THIS
 text = load_doc(filename)
-for line in text[:10]:
-    print(line,end='')
-    
     
 #Mapping image with captions using dictionary
 def image_to_captions(text):
     hash_map = {}
     for line in text:
         token = line.split()
+        image_id = token[0].split('.')[0] # separating with '.' to extract image id (removing .jpg)
+        image_caption = ' '.join(token[1: ])
+    
+    #Add imageID and Image caption to hash map
+        if(image_id not in hash_map):
+                hash_map[image_id] = [image_caption]
+        else:
+            hash_map[image_id].append(image_caption)
+        
+    return hash_map
+
+#Create a map img to captions object
+map_img_to_captions = image_to_captions(text)
+
+def preprocess(map_img_to_captions):
+    preprocessed_captions = []
+    for key in map_img_to_captions.keys():
+        for idx in range(len(map_img_to_captions[key])):
+            tokens = map_img_to_captions[key][idx].split()
+            tokens = [token.lower() for token in tokens if len(token)>1 if token.isalpha()]
+            map_img_to_captions[key][idx] = ' '.join(tokens)
+            
+    return map_img_to_captions
+
+preprocessed_map = preprocess(map_img_to_captions)
+
+def create_vocabulary(preprocessed_map):
+    vocabulary = set()
+    for img_captions in preprocessed_map.values(): # list of 5 captions for each image
+        for caption in img_captions:
+            for token in caption.split():
+                vocabulary.add(token)    
+    return print(type(vocabulary))
 
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Image Captioning Using Keras
+
+From:
+https://iq.opengenus.org/image-captioning-using-keras/
+
+Stopped because the level became too advanced for me at this stage of my learning journey. I'll come back to it later.
+
